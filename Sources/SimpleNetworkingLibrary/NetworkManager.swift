b92debbc684bbc8736d5dfcde6fc00482bd5a910
Swayambhu Jyoti Banerjee <swayambhu.2010@gmail.com>
2026-02-04 14:43:36 +0530

    Made convenience initializer for simplicity

--- Sources/SimpleNetworkingLibrary/NetworkManager.swift
+++ Sources/SimpleNetworkingLibrary/NetworkManager.swift
@@ -23,9 +23,17 @@ public class NetworkService: NetworkRequest {
         self.decoder = decoder
     }
     
+    public convenience init() {
+        self.init(
+            requestBuilder: RequestBuilder(),
+            sessionManager: RequestSession(),
+            decoder: ResponseObject()
+        )
+    }
+    
     public func request<T: Decodable>(apiRequest: APIEndPoint) async throws -> T? {
         guard let url = requestBuilder.createRequest(request: apiRequest) else { return nil }
-        let (data, response) = try await sessionManager.execute(url: url)
+        let (data, _) = try await sessionManager.execute(url: url)
         let obj = try decoder.decodeResponse(type: T.self, data: data)
         return obj
     }
